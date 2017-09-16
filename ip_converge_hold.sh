44f121584ee43f0dbd332b12d6467c27634e3466
ashmastaflash <ash.d.wilson@gmail.com>
2017-09-16 16:40:45 -0700

    chg: dev: Adding IP converge hold script. !minor

---
+++ ip_converge_hold.sh
@@ -0,0 +1,12 @@
+for (( ; ; ))
+do
+  echo "Checking convergence of IP addresses..."
+  echo "My IP is ${MY_IP}"
+  echo "DNS says my IP is `host ${DNS_NAME} | awk '{print $4}'`"
+  if [ `host ${DNS_NAME} | awk '{print $4}'` == "${MY_IP}"]; then
+    echo "IP addresses converged. we can continue..."
+    exit 0
+  fi
+  echo "Will wait 30 seconds and check again."
+  sleep 30
+done
