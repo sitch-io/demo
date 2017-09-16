8e3f84350334aaec571e720ca7361e3cd6889566
ashmastaflash <ash.d.wilson@gmail.com>
2017-09-16 16:43:09 -0700

    chg: dev: Missing space in if statement. !minor

--- ip_converge_hold.sh
+++ ip_converge_hold.sh
@@ -3,7 +3,7 @@ do
   echo "Checking convergence of IP addresses..."
   echo "My IP is ${MY_IP}"
   echo "DNS says my IP is `host ${DNS_NAME} | awk '{print $4}'`"
-  if [ `host ${DNS_NAME} | awk '{print $4}'` == "${MY_IP}"]; then
+  if [ `host ${DNS_NAME} | awk '{print $4}'` == "${MY_IP}" ]; then
     echo "IP addresses converged. we can continue..."
     exit 0
   fi
