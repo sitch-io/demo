b49c1918e6b71ddf03759bb90c171abedff21119
ashmastaflash <ash.d.wilson@gmail.com>
2017-09-16 17:45:01 -0700

    chg: dev: Installing docker-compose via separate script. !minor

--- debian/install-docker.sh
+++ debian/install-docker.sh
@@ -13,8 +13,3 @@ stable"
 apt-get update
 apt-get install docker-ce
 docker run hello-world
-
-echo "Installing Docker Compose (version $dockerComposeVersion)..."
-curl -L https://github.com/docker/compose/releases/download/$dockerComposeVersion/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-docker-compose --version
