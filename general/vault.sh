47950899e8ca3763690762b3640fa6646c818cac
ashmastaflash <ash.d.wilson@gmail.com>
2017-09-17 21:48:39 -0700

    chg: dev: do-si-do your env var. !minor

--- general/vault.sh
+++ general/vault.sh
@@ -27,6 +27,6 @@ echo "${warn}To be extra safe, use srm to delete these files permanently, once y
 for i in 1 2 3
 do
   set UNLOCK_KEY=`cat ../vault_unseal_keys | head -"$i" | tail -1`
-  echo ${UNOCK_KEY} | docker exec -it sitch_vault vault unseal --tls-skip-verify
+  docker exec -it sitch_vault vault unseal --tls-skip-verify < echo ${UNOCK_KEY}
   sleep 1
 done
